87d915863d1fa93d3ded70aa6f3e8b12f920a55a
ColeWoodford <woodford.cole@gmail.com>
2018-11-11 09:31:52 -0800

    Added content to homepage and news. Added a google calendar.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,9 +5,7 @@ class Calendar extends Component {
     return (
       <div>
         <h2>Calendar</h2>
-        <div>
-          Content for calendar goes here.
-        </div>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=colesslawtruck%40gmail.com&amp;color=%2329527A&amp;ctz=America%2FLos_Angeles" style={{borderWidth:"0",width:"100%",height:"900px",frameborder:"0",scrolling:"no"}}></iframe>
       </div>
     );
   }
