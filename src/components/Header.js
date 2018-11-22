100b8a4b2ea8720a6e1d498a296371bccf61ab80
Emmanuel Bonilla <emmanuel.bonilla@wsu.edu>
2018-11-22 00:05:06 -0800

    still working on header
    Changed both page header and navbar to have image. page header is using an .png image to test. made sure event keys still work

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,30 +8,57 @@ class Header extends Component {
         <PageHeader>
           <Panel>
             <a href="/">
-              <img src="images/2485_Logo-150x120.jpg" alt="2485Logo" style={{width:'150px',height:'120px', position:"relative"}}/>
+              <img src="images/FOE_logo.png" alt="2485Logo" style={{width:'150px',height:'120px', position:"relative"}}/>
             </a>
-            Fraternal Order of Eagles <small>Kennewick #2485</small>
+
+            <Nav bsStyle="pills">
+              <NavItem class="nav justify-content-end" eventKey={1} href="/">
+                Home
+              </NavItem>
+              <NavItem eventKey={1} href="/membership">
+                Membership
+              </NavItem>
+              <NavItem eventKey={2} href="/news">
+                News
+              </NavItem>
+              <NavItem eventKey={3} href="/calendar">
+                Calendar
+              </NavItem>
+              <NavItem eventKey={4} href="/directions">
+                Directions
+              </NavItem>
+              <NavItem eventKey={5} href="/contact">
+                Contact Us
+              </NavItem>
+            </Nav>
+            
           </Panel>
         </PageHeader>
+
         <Navbar>
-          <Nav>
-            <NavItem eventKey={1} href="/">
-              Home
-            </NavItem>
-            <NavItem eventKey={1} href="/membership">
-              Membership
-            </NavItem>
-            <NavItem eventKey={2} href="/news">
-              News
-            </NavItem>
-            <NavItem eventKey={3} href="/calendar">
-              Calendar
-            </NavItem>
-            <NavItem eventKey={4} href="/directions">
-              Directions
-            </NavItem>
-            <NavItem eventKey={5} href="/contact">
-              Contact Us
+            <Nav>
+              <NavItem>
+                <a href="/">
+                  <img src="images/2485_Logo-150x120.jpg" alt="2485Logo" style={{width:'150px',height:'120px', position:"relative"}}/>
+                </a>
+              </NavItem>
+              <NavItem eventKey={1} href="/">
+                Home
+              </NavItem>
+              <NavItem eventKey={1} href="/membership">
+                Membership
+              </NavItem>
+              <NavItem eventKey={2} href="/news">
+                News
+              </NavItem>
+              <NavItem eventKey={3} href="/calendar">
+                Calendar
+              </NavItem>
+              <NavItem eventKey={4} href="/directions">
+                Directions
+              </NavItem>
+              <NavItem eventKey={5} href="/contact">
+                Contact Us
             </NavItem>
           </Nav>
         </Navbar>
